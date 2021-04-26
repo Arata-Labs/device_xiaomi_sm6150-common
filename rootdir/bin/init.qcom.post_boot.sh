2b3f8fb9a2c5a19d444479d25b416d408c2f33e4
Subhajeet Muhuri <subhajeet.muhuri@aosip.dev>
2021-04-26 10:26:34 +0530

    sm6150-common: rootdir: Drop sched_{down, up}migrate_boosted tunings
     * Relevant kernel patches also have been dropped
    
    Signed-off-by: Subhajeet Muhuri <subhajeet.muhuri@aosip.dev>
    Change-Id: I3c781f901efdcdf07c4fcf11a3a48bd4c9542543
    Signed-off-by: HinohArata <mmgcntk@gmail.com>

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -45,8 +45,6 @@ case "$soc_id" in
         "355" | "369" | "377" | "380" | "384" )
 
     # Setting b.L scheduler parameters
-    echo 25 > /proc/sys/kernel/sched_downmigrate_boosted
-    echo 25 > /proc/sys/kernel/sched_upmigrate_boosted
     echo 85 > /proc/sys/kernel/sched_downmigrate
     echo 95 > /proc/sys/kernel/sched_upmigrate
 
@@ -146,8 +144,6 @@ case "$soc_id" in
     "365" | "366" )
 
     # Setting b.L scheduler parameters
-    echo 25 > /proc/sys/kernel/sched_downmigrate_boosted
-    echo 25 > /proc/sys/kernel/sched_upmigrate_boosted
     echo 85 > /proc/sys/kernel/sched_downmigrate
     echo 95 > /proc/sys/kernel/sched_upmigrate
 

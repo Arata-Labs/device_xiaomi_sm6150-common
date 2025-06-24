3def163de8cbeb3d80408a83eb9de46d622a7e00
HinohArata <mmgcntk@gmail.com>
2025-06-25 01:54:05 +0700

    sm6150-common: Remove unneeded packages during build
    Signed-off-by: HinohArata <mmgcntk@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -281,6 +281,10 @@ PRODUCT_PACKAGES += \
     init.target.rc \
     ueventd.qcom.rc
 
+# Remove Packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # Sensors
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.accelerometer.xml \
